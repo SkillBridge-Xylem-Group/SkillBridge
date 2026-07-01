6143e66c730d1aac7b8092890db089dcf2a7f628
Nameless <j13787810907@163.com>
2026-07-02 01:14:11 +0700

    Revert "Add landing pages"

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [{ hostname: "images.unsplash.com" }],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
